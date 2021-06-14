eb87117517507a94eeae473a057dcd50d4327cf9
Pascal Pater <pascal.pater@gmail.com>
2021-06-14 16:21:15 +0200

    set AppProvider and return song overview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
-import './App.css';
+import { AppProvider } from "./components/AppContext";
+import SongOverview from "./components/SongOverview";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hallo React Playlist</p>
-    </div>
+    <AppProvider>
+      <div className="App">
+        <p>Hallo React Playlist from App</p>
+        <SongOverview />
+      </div>
+    </AppProvider>
   );
 }
 
