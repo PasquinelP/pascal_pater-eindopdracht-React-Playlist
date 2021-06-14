14452211ae3314f96a154f37a36a8590a8beeb02
Pascal Pater <pascal.pater@gmail.com>
2021-06-14 16:22:32 +0200

    create basic SongOverview component and import SongList component to see if everything works

---
+++ src/components/SongOverview.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SongForm from './SongForm';
+import SongList from './SongList';
+
+const SongOverview = () => {
+
+
+  return (
+    <div>
+      <h1>Song overview</h1>
+      {/* <SongForm /> */}
+      <SongList />
+    </div>
+  );
+
+}
+
+export default SongOverview;
