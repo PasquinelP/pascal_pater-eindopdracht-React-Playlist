6a59dcef6203a05d04924f8f9a60ef264a05ba78
Pascal Pater <pascal.pater@gmail.com>
2021-06-14 16:23:43 +0200

    create SongList component with test content and import Song component

---
+++ src/components/SongList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Song from "./Song";
+
+const SongList = () => {
+ 
+  return (
+    <div>
+      <h3>Songlist: </h3>
+      <ul>
+        <Song />
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
