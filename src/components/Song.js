72d6ba24b83297f01a7d2b29482c1cc9012b95de
Pascal Pater <pascal.pater@gmail.com>
2021-06-14 21:46:38 +0200

    add key to each song in the list

--- src/components/Song.js
+++ src/components/Song.js
@@ -5,7 +5,7 @@ const Song = () => {
   const { songs } = useContext(AppContext);
 
   const song = songs.map((song) => (
-    <tr>
+    <tr key={song.id}>
       <td>{song.title}</td>
       <td>{song.artist}</td>
       <td>{song.genre}</td>
