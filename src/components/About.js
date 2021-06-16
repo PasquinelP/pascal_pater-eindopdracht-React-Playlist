dcb4426a723c7a4763ae12b1c36aa608b0ad05d5
Pascal Pater <pascal.pater@gmail.com>
2021-06-16 19:49:31 +0200

    add text to About page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,22 @@ import React from "react";
 
 const About = () => {
   return (
-    <h1>About</h1>
+    <div className="about-page">
+      <p>
+        Ik ben een enthousiaste front-end developer met meerdere jaren
+        werkervaring in voornamelijk HTML, CSS, SCSS, BEM en responsive design.
+      </p>
+
+      <p>
+        Momenteel volg ik een opleiding front-end development bij Winc Academy
+        om mijn kennis uit te breiden met o.a. Javascript en React.
+      </p>
+
+      <p>
+        Deze React Playlist is mijn eerste eindopdracht van de module React. De
+        focus van deze opdracht ligt voor 100% op het leren en toepassen van React en JavaScript en het kunnen maken van de gevraagde functionaliteit. Vandaar dat vanwege de tijd minimale aandacht is besteed aan de styling.
+      </p>
+    </div>
   );
 };
 
