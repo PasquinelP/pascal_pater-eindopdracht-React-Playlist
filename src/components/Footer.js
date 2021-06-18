1cf5cdb4c131b2be795308af840d46d061e9d758
Pascal Pater <pascal.pater@gmail.com>
2021-06-18 11:39:30 +0200

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Pascal Pater - Eindopdracht React Playlist voor Winc
+        Academy Front-end Development opleiding
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
