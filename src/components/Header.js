7294be694df5446c29bf6880991b936f9c0e0c07
Pascal Pater <pascal.pater@gmail.com>
2021-06-18 11:39:15 +0200

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="page-header">
+      <h1>React Playlist</h1>
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
