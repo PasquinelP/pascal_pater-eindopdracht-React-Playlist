619e0a74f3b424bf1f108039ccbac51e467e12da
Pascal Pater <pascal.pater@gmail.com>
2021-06-18 11:11:59 +0200

    add some extra songs for testing

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -7,16 +7,44 @@ export const AppProvider = (props) => {
   const [songs, setSongs] = useState([
     {
       id: 1,
-      title: "All Shook Up",
-      artist: "Elvis Presley",
-      genre: "Rock 'n Roll",
-      rating: 5,
+      title: "Not an addict",
+      artist: "K's Choice",
+      genre: "Rock",
+      rating: 3,
     },
     {
       id: 2,
+      title: "All Shook Up",
+      artist: "Elvis Presley",
+      genre: "Rock and Roll",
+      rating: 5,
+    },
+    {
+      id: 3,
       title: "The thrill is gone",
       artist: "B.B. King",
       genre: "Blues",
+      rating: 4,
+    },
+    {
+      id: 4,
+      title: "Every Breath You Take",
+      artist: "The Police",
+      genre: "Pop",
+      rating: 3,
+    },
+    {
+      id: 5,
+      title: "Baby Love",
+      artist: "Mother's Finest",
+      genre: "Rock",
+      rating: 3,
+    },
+    {
+      id: 6,
+      title: "Jackson",
+      artist: "Johnny Cash",
+      genre: "Country",
       rating: 4,
     },
   ]);
