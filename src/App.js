f8efbaf1b5e0ef8520b54b167c1f2c818f5b9446
Pascal Pater <pascal.pater@gmail.com>
2021-06-21 17:27:45 +0200

    move about into pages folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { AppProvider } from "./components/AppContext";
 import Header from "./components/Header";
 import SongOverview from "./components/SongOverview";
-import About from "./components/About";
+import About from "./components/pages/About";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
