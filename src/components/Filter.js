2b16be9ed103b84dc559e57f1b7c75c6783ca998
Pascal Pater <pascal.pater@gmail.com>
2021-06-21 16:18:35 +0200

    add rating filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,18 +3,20 @@ import useApp from "./useApp";
 
 const Filter = () => {
   
-  const { filteredGenre, handleGenreChange } = useApp();
+  const { filteredGenre, filteredRating, handleGenreChange, handleRatingChange } = useApp();
 
 
   return (
     <div className="filter-options">
+
+      {/* Filter genre */}
       <label htmlFor="filteredGenre">Filter genre:</label>
       <select
         value={filteredGenre}
         onChange={handleGenreChange}
         name="filteredGenre"
       >
-        <option value="">All genres</option>
+        <option value="All">All genres</option>
         <option value="Blues">Blues</option>
         <option value="Country">Country</option>
         <option value="HipHop">HipHop</option>
@@ -25,6 +27,22 @@ const Filter = () => {
         <option value="Rock and Roll">Rock and Roll</option>
         <option value="Rock">Rock</option>
       </select>
+
+      {/* Filter rating */}
+      <label htmlFor="filteredRating">Filter rating:</label>
+      <select
+        value={filteredRating}
+        onChange={handleRatingChange}
+        name="filteredRating"
+      >
+        <option value="All">All ratings</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      
     </div>
   );
 };
